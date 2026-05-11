e22ac6ebb755ab166d96dac3576e021ac53ad63c
Subhanjeet <ssubhanjeet@gmail.com>
2026-05-11 22:37:19 +0530

    initail commit

---
+++ PYTHON/Conditions/vote.py
@@ -0,0 +1,7 @@
+name = input("enter your name: ")
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print(f"{name}, your are eligible to vote")
+else:
+    print(f"{name}, your are not eligible to vote")                
